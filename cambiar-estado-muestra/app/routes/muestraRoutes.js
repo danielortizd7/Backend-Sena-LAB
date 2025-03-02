64f9ef0d7205a43950936543b46d40d401844bea
daniel <danielortizd7mail.com>
2025-03-01 21:23:23 -0500

    Eliminando carpetas para dejar estructura limpia

--- cambiar-estado-muestra/app/routes/muestraRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { actualizarEstado } = require("../controllers/muestraController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-router.put("/cambiar-estado", authMiddleware, actualizarEstado);
-
-module.exports = router;
